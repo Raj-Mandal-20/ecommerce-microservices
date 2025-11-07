5e119111f1605e6ca5c3adb50bf2ae2374c3bd24
Raj-Mandal-20 <usingrajcseinfo@gmail.com>
2025-11-07 23:24:30 +0530

    feat(inventory-service): add REST controller and DTOs for CRUD endpoints

---
+++ services/inventory-service/src/main/java/com/inventoryservice/inventory_service/dto/InventoryResponse.java
@@ -0,0 +1,16 @@
+package com.inventoryservice.inventory_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String productId;
+    private int quantity;
+    private String location;
+}
